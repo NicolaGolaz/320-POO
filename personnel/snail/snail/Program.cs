39aec14c1f4917bf9a15deeedb38c4f3f06fc02b
NicolaGolaz <nicola.golaz@eduvaud.ch>
2024-08-30 10:16:33 +0200

    exercice snail v1

---
+++ personnel/snail/snail/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+namespace snail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int intVie = 50;
+            const int intMaxPv = 50;
+            string strSnail = "_@_ö";
+
+            for (int i = 0; i < intMaxPv; i++)
+            {
+
+               
+                    Thread.Sleep(50);
+                    Console.Clear();
+                    Console.CursorVisible = false;
+                    Console.SetCursorPosition(i, 5);
+                    Console.WriteLine(strSnail);
+
+                    intVie = intVie - 1;
+                
+            }
+            Console.Clear() ;
+            Console.Set
+            strSnail = "____";
+            Console.WriteLine(strSnail);
+            Console.ReadLine();
+        }
+    }
+}
