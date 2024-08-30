ede8caf57dbaf75bdf2284baece29c4bd9c29e3e
NicolaGolaz <nicola.golaz@eduvaud.ch>
2024-08-30 12:05:22 +0200

    Work in progress

---
+++ personnel/parachutes/parachutes/Plane.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classPlane
+{
+    internal class Plane
+    {
+        // Attributs
+
+        private int x = 0;
+
+        private string[] view =
+    {
+            @" _                         ",
+            @"| \                        ",
+            @"|  \       ______          ",
+            @"--- \_____/  |_|_\____  |  ",
+            @"  \_______ --------- __>-} ",
+            @"        \_____|_____/   |  "
+        };
+
+
+        // Constructeur
+
+        // Méthodes 
+
+        public void Move()
+        {
+            x++;
+        }
+
+    }
+}
---
+++ personnel/parachutes/parachutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using classPlane;
+using classConfig;
+
+namespace parachutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Plane plane = new Plane();
+
+            while (true)
+            {
+                Console.SetCursorPosition(plane.x, 0);
+                
+            }
+
+        }
+    }
+}
