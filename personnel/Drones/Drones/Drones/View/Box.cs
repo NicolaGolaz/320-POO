214cac82f3df4bf36c474b396b140bee4ef29268
NicolaGolaz <nicola.golaz@eduvaud.ch>
2024-10-04 08:55:52 +0200

    feat (Drones) Ajout de la classe Box

---
+++ personnel/Drones/Drones/Drones/View/Box.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.View
+{
+    internal class Box
+    {
+        private int numBox;
+        private int kilos;
+        private Color color;
+    }
+}
